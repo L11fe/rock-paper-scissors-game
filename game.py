ebf49084aed5d89953c88c4a654c73c4d105233f
L11fe <114050712+L11fe@users.noreply.github.com>
2022-09-21 09:40:22 +0300

    Update game.py

--- game.py
+++ game.py
@@ -1,6 +1,10 @@
-human_turn = 'rock'
-computer_turn = 'scissors'
+import random
+turns = ['rock', 'paper', 'scissors']
 
+human_turn = input("Enter your turn = ")
+computer_turn = random.choice(turns)
+
+print(f'Human:{human_turn} vs. Computer:{computer_turn}')
 if human_turn == computer_turn:
     print('Draw')
 
